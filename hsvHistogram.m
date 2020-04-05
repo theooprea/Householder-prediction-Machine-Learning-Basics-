0e2b23214aa459bb4f7871878e51e179920ea6b2
theooprea <opreatheodor54@gmail.com>
2020-04-05 14:35:58 +0300

    Faster histograms

--- hsvHistogram.m
+++ hsvHistogram.m
@@ -2,17 +2,22 @@ function [sol] = hsvHistogram(path_to_image, count_bins)
   A = imread(path_to_image);
   A = transform_to_HSV(A);
   sol = zeros(1, 3 * count_bins);
-  step = 1.01/count_bins;
-    for i = 1:count_bins
-    binranges = [(i-1) * step i * step];
-    %for H
-    bincounts = histc(A(:,:,1), binranges);
-    sol(1, i) = sum(bincounts(1,:));
-    %for S
-    bincounts = histc(A(:,:,2), binranges);
-    sol(1, i + count_bins) = sum(bincounts(1,:));
-    %for V
-    bincounts = histc(A(:,:,3), binranges);
-    sol(1, i + 2 * count_bins) = sum(bincounts(1,:));
-  end
+  step = 1.01 / count_bins;
+  i = 1 : count_bins;
+  binranges = [(i-1) * step i * step];
+  bincounts_hue =  histc(A(:,:,1), binranges);
+  bincounts_saturation = histc(A(:,:,2), binranges);
+  bincounts_value = histc(A(:,:,3), binranges);
+  bincounts_hue = sum(bincounts_hue');
+  bincounts_saturation = sum(bincounts_saturation');
+  bincounts_value = sum(bincounts_value');
+  %pt rosu
+  sol(1, 1 : count_bins) = bincounts_hue(1, 1 : 2 : 2 * count_bins);
+  sol(1, 2 : count_bins) = sol(1, 2 : count_bins) + bincounts_hue(1, 2 : 2 : 2 * count_bins - 1);
+  %pt verde
+  sol(1, count_bins + 1 : 2 * count_bins) = bincounts_saturation(1, 1 : 2 : 2 * count_bins);
+  sol(1, count_bins + 2 : 2 * count_bins) = sol(1, count_bins + 2 : 2 * count_bins) + bincounts_saturation(1, 2 : 2 : 2 * count_bins - 1);
+  %pt albastru
+  sol(1, 2 * count_bins + 1 : 3 * count_bins) = bincounts_value(1, 1 : 2 : 2 * count_bins);
+  sol(1, 2 * count_bins + 2 : 3 * count_bins) = sol(1, 2 * count_bins + 2 : 3 * count_bins) + bincounts_value(1, 2 : 2 : 2 * count_bins - 1);
 end
--- rgbHistogram.m
+++ rgbHistogram.m
@@ -2,16 +2,21 @@ function [sol] = rgbHistogram(path_to_image, count_bins)
   A = imread(path_to_image);
   sol = zeros(1, 3 * count_bins);
   step = 256/count_bins;
-  for i = 1:count_bins
-    binranges = [(i-1) * step i * step];
-    %for red channel
-    bincounts = histc(A(:,:,1), binranges);
-    sol(1, i) = sum(bincounts(1,:));
-    %for green channel
-    bincounts = histc(A(:,:,2), binranges);
-    sol(1, i + count_bins) = sum(bincounts(1,:));
-    %for blue channel
-    bincounts = histc(A(:,:,3), binranges);
-    sol(1, i + 2 * count_bins) = sum(bincounts(1,:));
-  end  
+  i = 1 : count_bins;
+  binranges = [(i-1) * step i * step];
+  bincounts_red =  histc(A(:,:,1), binranges);
+  bincounts_green = histc(A(:,:,2), binranges);
+  bincounts_blue = histc(A(:,:,3), binranges);
+  bincounts_red = sum(bincounts_red');
+  bincounts_green = sum(bincounts_green');
+  bincounts_blue = sum(bincounts_blue');
+  %pt rosu
+  sol(1, 1 : count_bins) = bincounts_red(1, 1 : 2 : 2 * count_bins);
+  sol(1, 2 : count_bins) = sol(1, 2 : count_bins) + bincounts_red(1, 2 : 2 : 2 * count_bins - 1);
+  %pt verde
+  sol(1, count_bins + 1 : 2 * count_bins) = bincounts_green(1, 1 : 2 : 2 * count_bins);
+  sol(1, count_bins + 2 : 2 * count_bins) = sol(1, count_bins + 2 : 2 * count_bins) + bincounts_green(1, 2 : 2 : 2 * count_bins - 1);
+  %pt albastru
+  sol(1, 2 * count_bins + 1 : 3 * count_bins) = bincounts_blue(1, 1 : 2 : 2 * count_bins);
+  sol(1, 2 * count_bins + 2 : 3 * count_bins) = sol(1, 2 * count_bins + 2 : 3 * count_bins) + bincounts_blue(1, 2 : 2 : 2 * count_bins - 1);
 end
